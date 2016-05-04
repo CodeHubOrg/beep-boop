87b5c9514260efb71e8e74f2f810fe51c88ffc43
Dave Thomson <djthomson@gmail.com>
2016-05-04 20:55:21 +0100

    Add socket io

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
-var express = require('express');
-var path    = require("path");
-var app     = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require("path");
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname+'/public/index.html'));
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+io.on('connection', function(socket){
+  console.log('a robot connected');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('emit:message: ' + msg);
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
 });
